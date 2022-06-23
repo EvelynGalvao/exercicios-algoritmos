b57c718065dad248cac3729ea32410e7b16e11eb
EvelynGalvao <evilenluhana@gmail.com>
2022-06-22 21:32:08 -0300

    commit on master

---
+++ src/Exercicio1.java
@@ -0,0 +1,11 @@
+public class Exercicio1 {
+
+    public static void main (String[] args) {
+
+
+        for(int i = 0; i <= 20; i++){
+            System.out.println("eu gosto de estudar algorítmos");
+
+        }
+    }
+}
