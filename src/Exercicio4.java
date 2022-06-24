5fc3239d28b72096dd82392f15bd7077e8bb3eb9
EvelynGalvao <evilenluhana@gmail.com>
2022-06-24 14:48:50 -0300

    commit on master

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+
 public class Exercicio4 {
 
     public static void main(String[] args) {
+        // crie as seguintes funções:
+        // TOTALPARES: a função recebe como parâmetro um array de inteiros e retorna um numero inteiro indicando
+        // um total de numeros pares existentes no array.
 
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+        numeros.add(54);
+        numeros.add(48);
+        numeros.add(2);
+        numeros.add(75);
+        numeros.add(10);
+        numeros.add(15);
+        numeros.add(1);
+
+        System.out.println(totalPares(numeros));
+    }
+
+    public static int totalPares(ArrayList<Integer> numeros) {
+
+        int pares = 0;
+
+        for (int i = 0; i < numeros.size(); i++) {
+            if (numeros.get(i) % 2 == 0) {
+                pares++;
+            }
+        }
+        return pares;
     }
 }
+
+
+// MAIORVALOR: a função recebe como parâmetro um array de inteiros e retorna o maior número existente no array.
+// TOTALIGUAIS: a função recebe como parâmetro um array de inteiros e retorna o total números iguais existentes em ambos
+//os arrays.
---
+++ src/Exercicio5.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Exercicio5 {
+    // MAIORVALOR: a função recebe como parâmetro um array de inteiros e retorna o maior número existente no array.
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        lista.add(54);
+        lista.add(48);
+        lista.add(2);
+        lista.add(75);
+        lista.add(10);
+        lista.add(15);
+        lista.add(1);
+
+        System.out.println(acharMaior(lista));
+    }
+
+    public static int acharMaior(ArrayList<Integer> lista) {
+
+        int maior = 0;
+
+        for (int i = 0; i < lista.size(); i++) {
+
+            if (lista.get(i) > maior) {
+                maior = lista.get(i);
+            }
+        }
+        return maior;
+
+    }
+}
