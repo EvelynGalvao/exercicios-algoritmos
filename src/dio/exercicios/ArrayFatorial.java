7b0abceada413d33b019dfd608b493d40c576baa
EvelynGalvao <evilenluhana@gmail.com>
2022-07-26 15:59:44 -0300

    commit on master

--- src/dio/exercicios/ArrayFatorial.java
+++ src/dio/exercicios/ArrayFatorial.java
@@ -1,4 +1,23 @@
 package dio.exercicios;
 
+import java.util.Scanner;
+
 public class ArrayFatorial {
+// criar um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! =");
+        for( int i = fatorial; i >= 1; i-- ){
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+    }
 }
